0f9b7d880b5911fda3708667144899c8200c6b5e
AzureAD\SteveBrooks <steve.brooks@tigerpistol.com>
2023-12-20 09:35:50 -0500

    solution

---
+++ PathFinder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChipSecuritySystem
+{
+    public class PathFinder
+    {
+        private List<ColorChip> colorChips;
+        private List<ColorChip> longestPath;
+
+        public PathFinder(List<ColorChip> colorChips)
+        {
+            this.colorChips = colorChips;
+            longestPath = new List<ColorChip>();
+        }
+
+        public List<ColorChip> FindLongestPath(Color startColor, Color endColor)
+        {
+            List<ColorChip> currentPath = new List<ColorChip>();
+            DFS(startColor, endColor, currentPath);
+            if (longestPath.Count == 0)
+            {
+                throw new InvalidOperationException("No valid path exists.");
+            }
+            return longestPath;
+        }
+
+        private void DFS(Color currentColor, Color endColor, List<ColorChip> currentPath)
+        {
+            foreach (var chip in colorChips.Where(c => c.StartColor == currentColor))
+            {
+                if (!currentPath.Contains(chip))
+                {
+                    currentPath.Add(chip);
+
+                    if (chip.EndColor == endColor)
+                    {
+                        // Update the longest path if the current path is longer
+                        if (currentPath.Count > longestPath.Count)
+                        {
+                            longestPath = new List<ColorChip>(currentPath);
+                        }
+                    }
+                    else
+                    {
+                        DFS(chip.EndColor, endColor, currentPath);
+                    }
+
+                    // Backtrack
+                    currentPath.Remove(chip);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChipSecuritySystem
+{
+    internal class Program
+    {
+        private static readonly List<ColorChip> Chips = new List<ColorChip>();
+        private static readonly Random Rand = new Random();
+
+        private static void Main()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter how many chips you would like to generate.");
+
+            int option;
+            while (!int.TryParse(Console.ReadLine(), out option) || (option < 2))
+            {
+                Console.WriteLine("Invalid input. Please enter a number greater than 2.");
+            }
+            
+            GenerateRandomChips(option);
+
+            Console.WriteLine("\nYour Chips\n----------\n");
+
+            foreach (var chip in Chips)
+            {
+                Console.WriteLine("[" + chip + "]");
+            }
+            Console.WriteLine();
+            try
+            {
+                PathFinder pathFinder = new PathFinder(Chips);
+
+                Color startColor = Color.Blue;
+                Color endColor = Color.Green;
+
+                List<ColorChip> longestPath = pathFinder.FindLongestPath(startColor, endColor);
+                Console.WriteLine("Longest Path:");
+                foreach (var chip in longestPath)
+                {
+                    Console.WriteLine($"{chip.StartColor} -> {chip.EndColor}");
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            Console.ReadLine(); // stop to see answer
+        }
+
+        private static void GenerateRandomChips(int n)
+        {
+            for (var i = 0; i < n; i++)
+            {
+                var startColor = (Color)Rand.Next(0, 6);
+                var endColor = (Color)Rand.Next(0, 6);
+                Chips.Add(new ColorChip(startColor, endColor));
+            }
+        }
+    }
+}
